1b1e502797e02ffe1dd80a9deb34b0d7e22ff6da
chrislopsa <christian_lopez09@hotmail.com>
2024-09-27 16:54:22 -0500

    feat(database): add PostgreSQL connection using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port:  5432,
+      username: 'username',
+      password: 'bdpasswordpsql',
+      database:'library_api',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }), BooksModule],
   controllers: [],
   providers: [],
 })
